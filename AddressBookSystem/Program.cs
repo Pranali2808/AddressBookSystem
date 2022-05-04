5f34bc2dd7c617061c51af4b2c84b348de929c6a
pranali2808 <ranepranali1997@gmail.com>
2022-05-04 17:16:30 +0530

    create contact to addressbook

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -11,6 +11,10 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to addressbook program");
+            // UC1 : Ability to create a Contacts in Address Book with first and last names, address,
+            // city, state, zip, phone number and email...
+            AddressBook.Display();
+            AddressBook.AddContact();
             Console.ReadLine();
         }
     }
